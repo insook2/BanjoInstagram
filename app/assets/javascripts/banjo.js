4a08dc7b24347613f0cee6e355244760e506e1aa
Insook choe <insook.choe@gmail.com>
2012-09-08 23:45:45 -0700

    set interval and code clean up

--- app/assets/javascripts/banjo.js
+++ app/assets/javascripts/banjo.js
@@ -1,95 +1,9 @@
-
-//window.Banjo = Backbone.Model.extend();
-//
-//window.BanjoCollection = Backbone.Collection.extend({
-//    model:Banjo,
-//    url:'/search'
-//});
-//
-//window.BanjoView = Backbone.View.extend({
-//      initialize: function(){
-//          _.bindAll(this.render);
-//          this.render();
-//      },
-//
-//    render:function(){
-//
-//        console.log('did we get here');
-//        this.$('body').html('<h1>Hi there </h1>');
-//
-//        return this;
-//    }
-//});
-//
-//var AppRouter = Backbone.Router.extend({
-//
-//    routes: {
-//        "": "home"
-//    },
-//
-//    home: function() {
-//        console.log("default start");
-//
-//        this.banjoList = new BanjoCollection();
-//        this.banjoListView = new BanjoView({model:this.banjoList});
-//        this.banjoList.fetch();
-//
-//    }
-//
-//});
-//
-//var app = new AppRouter();
-//Backbone.history.start();
-
-////Model
-//window.Banjo = Backbone.Model.extend();
-//
-//window.BanjoCollection = Backbone.Collection.extend({
-//    model:Banjo,
-//    url:'/search'
-//});
-//
-//window.BanjoView = Backbone.View.extend({
-//      el: $('#main_container #instagram'),
-//      initialize: function(){
-//          _.bindAll(this.render);
-//
-//          this.render();
-//      },
-//
-//    render:function(){
-//       $(this.el).html(this.model.toJSON());
-//        return this;
-//    }
-//});
-//
-//var AppRouter = Backbone.Router.extend({
-//    routes:{
-//        "":"list"
-//    },
-//    list:function(){
-//      this.banjoList = new BanjoCollection();
-//      this.banjoListView = new BanjoListView({model:this.banjoList});
-//      this.banjoList.fetch();
-//
-//    }
-//});
-
 //Model
 window.Banjo = Backbone.Model.extend();
 
 window.BanjoCollection = Backbone.Collection.extend({
     model:Banjo,
     url:'/search'
-//    sync: function(options) {
-//        $.ajax({
-//            type: "GET",
-//            url: '/search'
-//        });
-//    },
-//    parse: function(response) {
-//        return response.data;
-//    }
 });
 
 window.BanjoView = Backbone.View.extend({
@@ -99,25 +13,9 @@ window.BanjoView = Backbone.View.extend({
           this.collection = new BanjoCollection();
           this.collection.bind('reset', this.render, this);
           this.resetCollection();
-//          this.model.fetch();
-//          this.render();
       },
 
     render:function(){
-//        $.ajax({
-//            type: "GET",
-//            url: '/search'
-//        }).done( function(response){
-//            popular = response["data"];
-//            responseData = popular;
-//            $('div#instagram').html('');
-//            for (var i = 0; i < popular.length; i++)
-//            {
-//                var imageurl = popular[i]["images"]["low_resolution"]["url"];
-//                $('div#instagram').append("<div class='image_holder '><img src ='" + imageurl + "'/></div> ") ;
-//            }
-//        });
-
         if( this.collection.length > 0)
         {
             $('div#instagram').html('');
@@ -132,7 +30,6 @@ window.BanjoView = Backbone.View.extend({
                 $('div#instagram').append("<div class='image_holder '><img src ='" + imageurl + "'/><span>"+captionText+"</span></div> ") ;
             }
         }
-
     },
 
     resetCollection: function(){
@@ -149,35 +46,11 @@ window.BanjoView = Backbone.View.extend({
 
 $(document).ready(function(){
    var view = new BanjoView();
+    interval = window.setInterval(function(){
+        var view = new BanjoView();
+    }, 30000);
+
+//    $('a').click(function(){
+//           clearInterval(interval);
+//        });
 });
-
-
-//window.Banjo = {
-//  Models: {},
-//  Collections: {},
-//  Views: {},
-//  Routers: {},
-//  init: function() {
-//      this.render();
-//  },
-//
-//  render: function(){
-//
-//      $.ajax({
-//          type: "GET",
-//          url: '/search'
-//      }).done( function(response){
-//              popular = response["data"];
-//              $('div#instagram').html('');
-//              for (var i = 0; i < popular.length; i++)
-//              {
-//                  var imageurl = popular[i]["images"]["low_resolution"]["url"];
-//                  $('div#instagram').append("<div class='image_holder '><img src ='" + imageurl + "'/></div> ") ;
-//              }
-//          });
-//  }
-//};
-//
-//$(document).ready(function(){
-//   Banjo.init();
-//});
